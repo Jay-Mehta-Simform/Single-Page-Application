1c2a6fe4e78815eeeade31e83bdd7c462923c269
Jay Mehta <jay.mehta@simformsolutions.com>
2023-04-07 19:25:42 +0530

    [Started With Carousel]

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 let opened = false;
+
+let icon = document.querySelector(".icon");
+let pin = document.querySelector(".pin");
+let ul = document.querySelector(".navigators");
+let gif = document.querySelector(".gif");
+let descriptionA = document.querySelector(".a");
+let descriptionB = document.querySelector(".b");
+let carousel = document.querySelector(".container");
+let body = document.querySelector("body");
+let prev = document.querySelector(".leftArrow");
+let next = document.querySelector(".rightArrow");
+let images = document.querySelectorAll(".image");
+
 setTimeout(() => {
 	if (!opened) {
 		console.log("Click On The Dragon Icon To Open Menu");
@@ -6,51 +19,53 @@ setTimeout(() => {
 	}
 }, 2000);
 
-let icon = document.querySelector(".icon");
-let pin = document.querySelector(".pin");
-let ul = document.querySelector(".navigators");
-let gif = document.querySelector(".gif");
-let description = document.querySelector(".description");
+icon.addEventListener("click", throwIcon);
+
+icon.addEventListener("animationend", () => {
+	icon.style.animation = "";
+	pin.style.transform = "rotate(90deg)";
+	ul.style.visibility = "visible";
+	setTimeout(() => (pin.style.visibility = "hidden"), 1500);
+});
+
+gif.addEventListener("mousedown", () => {
+	gif.classList.toggle("initial");
+	descriptionB.classList.toggle("animateBounce");
+	descriptionA.classList.toggle("animateBounce");
+});
+
+carousel.addEventListener("mousedown", (e) => {
+	isDragStart = true;
+	prevPageX = e.pageX;
+	prevScrollLeft = carousel.scrollLeft;
+});
+carousel.addEventListener("mousemove", scrollByDrag);
+body.addEventListener("mouseup", () => {
+	isDragStart = false;
+	carousel.classList.remove("dragging");
+});
 
 function throwIcon() {
 	console.log("here");
 	if (ul.style.visibility == "") {
 		opened = true;
 		pin.style.visibility = "visible";
-		icon.style.animation = "1s linear iconThrow, 1s linear iconAnimation";
+		icon.style.animation = "1s linear iconAnimation";
 		ul.style.animation = "2s linear 1s visible";
 	}
 	if (opened) icon.removeEventListener("click", throwIcon);
 }
 
-icon.addEventListener("click", throwIcon);
+let isDragStart = false,
+	prevPageX,
+	prevScrollLeft;
 
-icon.addEventListener("animationend", () => {
-	icon.style.animation = "";
-	pin.style.animation = "0.3s fall";
-});
-
-pin.onanimationend = () => {
-	pin.style.visibility = "hidden";
-	pin.style.animation = "";
-	ul.style.visibility = "visible";
-};
-
-let isAnimationOn = false;
-
-gif.addEventListener("click", () => {
-	if (!isAnimationOn) {
-		gif.classList.toggle("initial");
-		description.classList.toggle("animateBounce");
-		isAnimationOn = true;
+function scrollByDrag(e) {
+	if (isDragStart) {
+		carousel.classList.add("dragging");
+		carousel.scrollLeft = e.pageX;
+		carousel.scrollLeft = prevScrollLeft - (e.pageX - prevPageX);
 	}
-});
+}
 
-description.addEventListener("animationstart", () => {
-	gif.classList.toggle("initial");
-});
-
-description.addEventListener("animationend", () => {
-	description.classList.toggle("animateBounce");
-	isAnimationOn = false;
-});
+prev.addEventListener("click", () => {});
